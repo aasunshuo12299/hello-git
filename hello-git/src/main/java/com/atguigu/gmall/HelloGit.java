971bb0d7b06bf8a6bca4d5c0194455e14ec9852a
sunshuo <891165883@qq.com>
2020-06-09 10:07:58 +0800

    第二次更新

--- hello-git/src/main/java/com/atguigu/gmall/HelloGit.java
+++ hello-git/src/main/java/com/atguigu/gmall/HelloGit.java
@@ -3,5 +3,6 @@ package com.atguigu.gmall;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("第二次更新");
     }
 }
