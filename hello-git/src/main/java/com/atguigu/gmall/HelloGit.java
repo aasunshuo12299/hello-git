b29a80a987955e1c106801557e5b8b77f22b1685
sunshuo <891165883@qq.com>
2020-06-09 10:18:17 +0800

    我创建了一个分支

--- hello-git/src/main/java/com/atguigu/gmall/HelloGit.java
+++ hello-git/src/main/java/com/atguigu/gmall/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第二次更新");
+        System.out.println("我创建了一个分支");
     }
 }
